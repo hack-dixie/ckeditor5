b870f66215e00b179298daab887cbab11ac09109
Pablo Perez <pabloperz_92@hotmail.com>
2022-06-07 16:54:40 -0500

    Upgrade!

--- webpack.config.js
+++ webpack.config.js
@@ -1,91 +1,60 @@
-/**
- * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
- */
+// webpack.config.js
 
- 'use strict';
+'use strict';
 
- /* eslint-env node */
+const path = require( 'path' );
+const { styles } = require( '@ckeditor/ckeditor5-dev-utils' );
 
- const path = require( 'path' );
- const webpack = require( 'webpack' );
- const { bundler, styles } = require( '@ckeditor/ckeditor5-dev-utils' );
- const CKEditorWebpackPlugin = require( '@ckeditor/ckeditor5-dev-webpack-plugin' );
- const TerserPlugin = require( 'terser-webpack-plugin' );
+module.exports = {
+    // https://webpack.js.org/configuration/entry-context/
+    entry: './app.js',
 
- module.exports = {
-     devtool: 'source-map',
-     performance: { hints: false },
+    // https://webpack.js.org/configuration/output/
+    output: {
+        path: path.resolve( __dirname, 'dist' ),
+        filename: 'bundle.js'
+    },
 
-     entry: path.resolve('app.js'),
+    module: {
+        rules: [
+            {
+                test: /\.svg$/,
 
-     output: {
-         // The name under which the editor will be exported.
-         library: 'ClassicEditor',
+                use: [ 'raw-loader' ]
+            },
+            {
+                test: /\.css$/,
 
-         path: path.resolve( __dirname, 'build' ),
-         filename: 'ckeditor.js',
-         libraryTarget: 'umd',
-         libraryExport: 'default'
-     },
+                use: [
+                    {
+                        loader: 'style-loader',
+                        options: {
+                            injectType: 'singletonStyleTag',
+                            attributes: {
+                                'data-cke': true
+                            }
+                        }
+                    },
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: styles.getPostCssConfig( {
+                                themeImporter: {
+                                    themePath: require.resolve( '@ckeditor/ckeditor5-theme-lark' )
+                                },
+                                minify: true
+                            } )
+                        }
+                    }
+                ]
+            }
+        ]
+    },
 
-     optimization: {
-         minimizer: [
-             new TerserPlugin( {
-                 sourceMap: true,
-                 terserOptions: {
-                     output: {
-                         // Preserve CKEditor 5 license comments.
-                         comments: /^!/
-                     }
-                 },
-                 extractComments: false
-             } )
-         ]
-     },
+    // Useful for debugging.
+    devtool: 'source-map',
 
-     plugins: [
-         new CKEditorWebpackPlugin( {
-             // UI language. Language codes follow the https://en.wikipedia.org/wiki/ISO_639-1 format.
-             // When changing the built-in language, remember to also change it in the editor's configuration (src/ckeditor.js).
-             language: 'en',
-             additionalLanguages: 'all'
-         } ),
-         new webpack.BannerPlugin( {
-             banner: bundler.getLicenseBanner(),
-             raw: true
-         } )
-     ],
-
-     module: {
-         rules: [
-             {
-                 test: /\.svg$/,
-                 use: [ 'raw-loader' ]
-             },
-             {
-                 test: /\.css$/,
-                 use: [
-                     {
-                         loader: 'style-loader',
-                         options: {
-                             injectType: 'singletonStyleTag',
-                             attributes: {
-                                 'data-cke': true
-                             }
-                         }
-                     },
-                     {
-                         loader: 'postcss-loader',
-                         options: styles.getPostCssConfig( {
-                             themeImporter: {
-                                 themePath: require.resolve( '@ckeditor/ckeditor5-theme-lark' )
-                             },
-                             minify: true
-                         } )
-                     }
-                 ]
-             }
-         ]
-     }
- };
+    // By default webpack logs warnings if the bundle is bigger than 200kb.
+    performance: { hints: false }
+};
